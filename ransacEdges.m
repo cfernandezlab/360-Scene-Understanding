67dcd35b75cc8bcca489decdc71ea74d0191a2fe
Clara <you@example.com>
2018-08-27 19:33:09 +0200

    update

--- ransacEdges.m
+++ ransacEdges.m
@@ -5,18 +5,16 @@ bestInliers = [];
 
 for k=1:iterations
     
-    % posibles inliers 
     randomP = randperm(size(data,2));
     r1 = [ data(1,randomP(1)), data(2,randomP(1)), data(3,randomP(1)) ];
     r2 = [ data(1,randomP(2)), data(2,randomP(2)), data(3,randomP(2)) ];
    
     n=cross(r1,r2); 
     n=normr(n)';
+    cosTetha = n'*data; 
     
-    cosTetha = n'*data; % tetha=90º
-    
-    threshold = 0.008;  %cos(6.59*180/imH); 
-    inliers = abs(cosTetha) < threshold; %0.008;
+    threshold = 0.008;  
+    inliers = abs(cosTetha) < threshold; 
     outliers = find(~inliers);
     inliers = find(inliers);
     
--- ransacVp.m
+++ ransacVp.m
@@ -5,68 +5,36 @@ bestVPs = [];
 sum_OldPoints = 0;
 linesInliers = 0;
 
-normales = []; %Las normales de todas las lineas
-for i=1:length(data)
-   normales(:,i) = data(i).normal;  
-end
+normals = [data.normal];
 
 for k=1:iterations
-    % Posibles VP
-    randomP = randperm(size(normales,2));
-    n1 = normales(:,randomP(1));
-    n2 = normales(:,randomP(2));
-    n3 = normales(:,randomP(3));
+
+    randomP = randperm(size(normals,2));
+    n1 = normals(:,randomP(1));
+    n2 = normals(:,randomP(2));
+    n3 = normals(:,randomP(3));
     v1 = cross(n1,n2);
     v2 = cross(v1,n3);
     v3 = cross(v1,v2);
     v = [v1,v2,v3]';
     v = normr(v);
     
-    angle= abs(v*normales);
+    angle= abs(v*normals);
     [vpmin, id_vpmin] = min(angle);
     
-    threshold = abs(sin(0.05)); % 0.05
+    threshold = abs(sin(0.05)); 
     valido = find(vpmin < threshold);
     
     sum_NewPoints = 0;
-
     for j=1:length(valido)
         sum_NewPoints = sum_NewPoints + length(data(valido(j)).LinePoints);
-    end
+    end  
+    
     if sum_NewPoints > sum_OldPoints
         sum_OldPoints = sum_NewPoints;
         linesInliers = length(valido)/length(data);
-%         bestDirections(j).normal = normales(:,valido(j));
-%         bestDirections(j).vp = v(id_vpmin(valido(j)),:);
-%         bestDirections(j).LinePoints = data(valido(j)).LinePoints;
-%         bestDirections(j).inliersProp = linesInliers;
         bestVPs = v;
     end
-    
-    
-    
-%     if length(valido) > length(bestDirections)
-%         for j=1:length(valido)
-%             bestDirections(j).normal = normales(:,valido(j));
-%             bestDirections(j).vp = v(id_vpmin(valido(j)),:);
-%             bestDirections(j).LinePoints = data(valido(j)).LinePoints;
-%             bestVPs = v;
-%         end
-%         length(valido)
-%     elseif length(valido) == length(bestDirections)
-%         sum_NewPoints = 0;
-%         sum_OldPoints = 0;
-%         for j=1:length(valido)
-%             sum_NewPoints = sum_NewPoints + length(data(valido(j)).LinePoints);
-%             sum_OldPoints = sum_OldPoints + length(bestDirections(j).LinePoints);
-%         end
-%         if sum_NewPoints > sum_OldPoints
-%             bestDirections(j).normal = normales(:,valido(j));
-%             bestDirections(j).vp = v(id_vpmin(valido(j)),:);
-%             bestDirections(j).LinePoints = data(valido(j)).LinePoints;
-%             bestVPs = v;
-%         end
-%     end
     
 end
 
